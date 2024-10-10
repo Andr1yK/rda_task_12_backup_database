9164909ea943f876177bf815e1ee79a0701b5a4a
Andriy Kobetiak <andriy.k@mate.academy>
2024-10-10 19:49:37 +0300

    Solution

--- task.sh
+++ task.sh
@@ -1,1 +1,10 @@
 #! /bin/bash
+
+USER=$DB_USER
+PASSWORD=$DB_PASSWORD
+
+mysqldump -u "$USER" -p"$PASSWORD" --no-create-db ShopDB | \
+mysql -u "$USER" -p"$PASSWORD" ShopDBReserve
+
+mysqldump -u "$USER" -p"$PASSWORD" --no-create-db --no-create-info ShopDB | \
+mysql -u "$USER" -p"$PASSWORD" ShopDBDevelopment
